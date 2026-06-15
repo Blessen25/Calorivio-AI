351f34ed10ff0729d15c82a907dbcea0e390720f
Blessen25 <blessenvinoymathew25@gmail.com>
2026-06-15 05:21:15 +0300

    Started an AI project

--- calorivio-ai/src/app/page.tsx
+++ calorivio-ai/src/app/page.tsx
@@ -1,14 +1,10 @@
+import Sidebar from "@/components/sidebar/sidebar";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="min-h-screen flex items-center justify-center bg-gray-100">
-      <div className="text-center">
-        <h1 className="text-5xl font-bold">Calorivio AI</h1>
-        <p className="mt-4 text-gray-600">
-          AI Powered Nutrition & Meal Planning
-        </p>
-      </div>
-    </main>
+    <>
+      <Sidebar />
+    </>
   );
 }
