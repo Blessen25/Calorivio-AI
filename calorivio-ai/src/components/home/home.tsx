6f17417c5c657be1fd5b10c32c72eb34857bd2e2
Blessen25 <blessenvinoymathew25@gmail.com>
2026-06-17 13:02:58 +0300

    Commited Changes

---
+++ calorivio-ai/src/components/home/home.tsx
@@ -0,0 +1,22 @@
+import './home.css';
+
+const Home_Comp = () => {
+
+    return(
+    
+        <>
+
+            <div className="layout">
+                <div className="sidebar">
+
+                </div>
+                <div className="main_dashboard">
+
+                </div>
+            </div>
+        </>
+    )
+
+}
+
+export default Home_Comp;
---
+++ calorivio-ai/src/screens/home.tsx
@@ -0,0 +1,14 @@
+import Home_Comp from "../components/home/home"
+
+const Home = () => {
+
+    return(
+    
+        <>
+
+            <Home_Comp />
+        </>
+    )
+}
+
+export default Home;
